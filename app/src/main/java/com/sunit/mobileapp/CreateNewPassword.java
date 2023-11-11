42f4064d1c6ea5a495d2cacae225fb39ea98ad76
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-11-11 11:21:43 +0700

    commit 1

---
+++ app/src/main/java/com/sunit/mobileapp/CreateNewPassword.java
@@ -0,0 +1,14 @@
+package com.sunit.mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_password);
+    }
+}
