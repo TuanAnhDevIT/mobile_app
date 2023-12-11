46f36170b1abd30398d5a9b58f3b130d1aa99d21
nttt0201 <water.water.and.water.0201@gmail.com>
2023-12-12 03:25:39 +0700

    Settings UI

--- app/src/main/java/com/sunit/mobileapp/Activities/SettingTestActivity.java
+++ app/src/main/java/com/sunit/mobileapp/Activities/SettingTestActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sunit.mobileapp.R;
 
 public class SettingTestActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_test);
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setSelectedItemId(R.id.setting_menu);
