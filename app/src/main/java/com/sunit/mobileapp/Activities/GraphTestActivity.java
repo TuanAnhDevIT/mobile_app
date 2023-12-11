3456556bfc494ba3e67fd229e7cf141cc4929b9d
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-12-11 15:58:07 +0700

    fix(navbar)

--- app/src/main/java/com/sunit/mobileapp/Activities/GraphTestActivity.java
+++ app/src/main/java/com/sunit/mobileapp/Activities/GraphTestActivity.java
@@ -2,8 +2,11 @@ package com.sunit.mobileapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.sunit.mobileapp.MainActivity;
 import com.sunit.mobileapp.R;
 
 public class GraphTestActivity extends AppCompatActivity {
@@ -12,5 +15,30 @@ public class GraphTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_test);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.graph_menu);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home_menu) {
+                startActivity(new Intent(getApplicationContext(), MainHomeActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.graph_menu) {
+                return true;
+            } else if (itemId == R.id.location_menu) {
+                startActivity(new Intent(getApplicationContext(), GoogleMap.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.setting_menu) {
+                startActivity(new Intent(getApplicationContext(), SettingTestActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            }
+            return false;
+        });
     }
 }
--- app/src/main/java/com/sunit/mobileapp/Activities/SettingTestActivity.java
+++ app/src/main/java/com/sunit/mobileapp/Activities/SettingTestActivity.java
@@ -2,8 +2,10 @@ package com.sunit.mobileapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sunit.mobileapp.R;
 
 public class SettingTestActivity extends AppCompatActivity {
@@ -12,5 +14,31 @@ public class SettingTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_test);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.setting_menu);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home_menu) {
+                startActivity(new Intent(getApplicationContext(), MainHomeActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.graph_menu) {
+                startActivity(new Intent(getApplicationContext(), GraphTestActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.location_menu) {
+                startActivity(new Intent(getApplicationContext(), GoogleMap.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.setting_menu) {
+
+                return true;
+            }
+            return false;
+        });
     }
 }
